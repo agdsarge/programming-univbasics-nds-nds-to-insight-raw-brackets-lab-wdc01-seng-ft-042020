375aef02ad7e3397ecad381bccba437d5992196f
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-27 16:15:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 
 require 'pp'
 pp directors_database.class
-pp directors_database
+pp directors_database[0][:movies][0][:worldwide_gross]
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
